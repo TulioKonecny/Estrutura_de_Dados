106d465cdacf8daf344fe86e0a49fbcd6bf6d7be
Tulio Koneçny <61877782+TulioKonecny@users.noreply.github.com>
2023-06-30 11:46:42 -0300

    Main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "TreeExplorer.h"
+
+int main() {
+    // Cria uma instância do TreeExplorer
+    TreeExplorer explorer;
+
+    // Executa o explorador de árvore
+    explorer.run();
+
+    return 0;
+}
+
+
