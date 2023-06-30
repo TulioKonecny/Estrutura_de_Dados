bd24b8dd26d2c46a225bcf2c8d184e5fa5204554
Tulio Koneçny <61877782+TulioKonecny@users.noreply.github.com>
2023-06-30 11:39:14 -0300

    Implementação do Explorador
    Funcionalidades: construção das árvores, operações básicas na árvore, como inserção, remoção e busca de elementos,Informações sobre a árvore (altura, tamanho e se a árvore é completa ou perfeita),visualização da árvore, ordenações.

---
+++ TreeExplorer.h
@@ -0,0 +1,83 @@
+#ifndef TREE_EXPLORER_H
+#define TREE_EXPLORER_H
+
+#include "BinarySearchTree.h"
+#include <chrono>
+
+class TreeExplorer {
+public:
+    TreeExplorer();
+
+    // Executa o explorador de árvore
+    void run();
+
+private:
+    BinarySearchTree tree;
+    std::chrono::steady_clock::time_point startTime;
+
+    // Imprime o menu de opções
+    void printMenu() const;
+    
+    // Processa a opção selecionada pelo usuário
+    void processOption(int option);
+    
+    // Constrói a árvore a partir de um arquivo
+    void constructTreeFromFile();
+    
+    // Constrói a árvore a partir de entradas do usuário
+    void constructTreeFromInput();
+    
+    // Imprime informações sobre a árvore
+    void printTreeInfo() const;
+    
+    // Insere um elemento na árvore
+    void insertElement();
+    
+    // Remove um elemento da árvore
+    void removeElement();
+    
+    // Procura por um elemento na árvore
+    void searchElement();
+    
+    // Verifica se a árvore é completa
+    void checkCompleteTree() const;
+    
+    // Verifica se a árvore é perfeita
+    void checkPerfectTree() const;
+    
+    // Imprime a árvore em ordem de busca em largura (BFS)
+    void printBFS() const;
+    
+    // Converte uma árvore binária em uma lista e ordena usando Bubble Sort
+    void convertAndSortWithBubbleSort();
+    
+    // Converte uma árvore binária em uma lista e ordena usando Selection Sort
+    void convertAndSortWithSelectionSort();
+    
+    // Converte uma árvore binária em uma lista e ordena usando Insertion Sort
+    void convertAndSortWithInsertionSort();
+    
+    // Converte uma árvore binária em uma lista e ordena usando Shell Sort
+    void convertAndSortWithShellSort();
+    
+    // Ordena uma lista usando Bubble Sort
+    void bubbleSort(std::vector<int>& list);
+    
+    // Ordena uma lista usando Selection Sort
+    void selectionSort(std::vector<int>& list);
+    
+    // Ordena uma lista usando Insertion Sort
+    void insertionSort(std::vector<int>& list);
+    
+    // Ordena uma lista usando Shell Sort
+    void shellSort(std::vector<int>& list);
+    
+    // Imprime uma lista de inteiros
+    void printList(const std::vector<int>& list) const;
+    
+    // Imprime o tempo decorrido desde o início da execução
+    void printTimeElapsed();
+};
+
+#endif
+
