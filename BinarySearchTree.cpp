76a36c735a60ebeda5d6844126ac0792b0500b21
Tulio Koneçny <61877782+TulioKonecny@users.noreply.github.com>
2023-06-30 15:07:50 -0300

    Atualização com notação Húngara
    Atualização com notação Húngara

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,72 +1,72 @@
 
 #include "BinarySearchTree.h"
 
-BinarySearchTree::BinarySearchTree() : root(nullptr) {}
+BinarySearchTree::BinarySearchTree() : ptrRoot(nullptr) {}
 
 BinarySearchTree::~BinarySearchTree() {
     clear();
 }
 
 void BinarySearchTree::clear() {
-    clear(root);
-    root = nullptr;
+    clear(ptrRoot);
+    ptrRoot = nullptr;
 }
 
 // Limpa a árvore, liberando a memória ocupada pelos nós
-void BinarySearchTree::clear(TreeNode* node) {
-    if (node) {
-        clear(node->left);
-        clear(node->right);
-        delete node;
+void BinarySearchTree::clear(TreeNode* ptrNode) {
+    if (ptrNode) {
+        clear(ptrNode->ptrLeft);
+        clear(ptrNode->ptrRight);
+        delete ptrNode;
     }
 }
 
 // Insere um valor na árvore
 void BinarySearchTree::insert(int intValue) {
-    insert(root, intValue);
+    insert(ptrRoot, intValue);
 }
 
-void BinarySearchTree::insert(TreeNode*& node, int intValue) {
-    if (!node) {
-        node = new TreeNode(intValue);
-    } else if (intValue < node->data) {
-        insert(node->left, intValue);
-    } else if (intValue > node->data) {
-        insert(node->right, intValue);
+void BinarySearchTree::insert(TreeNode*& ptrNode, int intValue) {
+    if (!ptrNode) {
+        ptrNode = new TreeNode(intValue);
+    } else if (intValue < ptrNode->intData) {
+        insert(ptrNode->ptrLeft, intValue);
+    } else if (intValue > ptrNode->intData) {
+        insert(ptrNode->ptrRight, intValue);
     }
 }
 
 // Remove um valor da árvore
 bool BinarySearchTree::remove(int intValue) {
-    return remove(root, intValue);
+    return remove(ptrRoot, intValue);
 }
 
-bool BinarySearchTree::remove(TreeNode*& node, int intValue) {
-    if (!node) {
+bool BinarySearchTree::remove(TreeNode*& ptrNode, int intValue) {
+    if (!ptrNode) {
         return false;
-    } else if (intValue < node->data) {
-        return remove(node->left, intValue);
-    } else if (intValue > node->data) {
-        return remove(node->right, intValue);
+    } else if (intValue < ptrNode->intData) {
+        return remove(ptrNode->ptrLeft, intValue);
+    } else if (intValue > ptrNode->intData) {
+        return remove(ptrNode->ptrRight, intValue);
     } else {
-        if (!node->left && !node->right) {
-            delete node;
-            node = nullptr;
-        } else if (!node->left) {
-            TreeNode* temp = node;
-            node = node->right;
-            delete temp;
-        } else if (!node->right) {
-            TreeNode* temp = node;
-            node = node->left;
-            delete temp;
+        if (!ptrNode->ptrLeft && !ptrNode->ptrRight) {
+            delete ptrNode;
+            ptrNode = nullptr;
+        } else if (!ptrNode->ptrLeft) {
+            TreeNode* ptrTemp = ptrNode;
+            ptrNode = ptrNode->ptrRight;
+            delete ptrTemp;
+        } else if (!ptrNode->ptrRight) {
+            TreeNode* ptrTemp = ptrNode;
+            ptrNode = ptrNode->ptrLeft;
+            delete ptrTemp;
         } else {
-            TreeNode* minNode = node->right;
-            while (minNode->left) {
-                minNode = minNode->left;
+            TreeNode* ptrMinNode = ptrNode->ptrRight;
+            while (ptrMinNode->ptrLeft) {
+                ptrMinNode = ptrMinNode->ptrLeft;
             }
-            node->data = minNode->data;
-            return remove(node->right, minNode->data);
+            ptrNode->intData = ptrMinNode->intData;
+            return remove(ptrNode->ptrRight, ptrMinNode->intData);
         }
         return true;
     }
@@ -74,66 +74,66 @@ bool BinarySearchTree::remove(TreeNode*& node, int intValue) {
 
 // Procura um valor na árvore
 TreeNode* BinarySearchTree::search(int intValue) const {
-    return search(root, intValue);
+    return search(ptrRoot, intValue);
 }
 
-TreeNode* BinarySearchTree::search(TreeNode* node, int intValue) const {
-    if (!node || node->data == intValue) {
-        return node;
-    } else if (intValue < node->data) {
-        return search(node->left, intValue);
+TreeNode* BinarySearchTree::search(TreeNode* ptrNode, int intValue) const {
+    if (!ptrNode || ptrNode->intData == intValue) {
+        return ptrNode;
+    } else if (intValue < ptrNode->intData) {
+        return search(ptrNode->ptrLeft, intValue);
     } else {
-        return search(node->right, intValue);
+        return search(ptrNode->ptrRight, intValue);
     }
 }
 
 // Verifica se a árvore é completa
 bool BinarySearchTree::isComplete() const {
-    int size = getSize();
-    return isComplete(root, 0, size);
+    int intSize = getSize();
+    return isComplete(ptrRoot, 0, intSize);
 }
 
-bool BinarySearchTree::isComplete(TreeNode* node, int index, int size) const {
-    if (!node) {
+bool BinarySearchTree::isComplete(TreeNode* ptrNode, int intIndex, int intSize) const {
+    if (!ptrNode) {
         return true;
     }
-    if (index >= size) {
+    if (intIndex >= intSize) {
         return false;
     }
-    return isComplete(node->left, 2 * index + 1, size) && isComplete(node->right, 2 * index + 2, size);
+    return isComplete(ptrNode->ptrLeft, 2 * intIndex + 1, intSize) && isComplete(ptrNode->ptrRight, 2 * intIndex + 2, intSize);
 }
 
 // Verifica se a árvore é perfeita
 bool BinarySearchTree::isPerfect() const {
-    int height = getHeight();
-    int size = getSize();
-    return size == (1 << height) - 1;
+    int intHeight = getHeight();
+    int intIndex = getSize();
+    return intIndex == (1 << intHeight) - 1;
 }
 
 // Imprime a árvore em ordem de largura (BFS)
 void BinarySearchTree::printBFS() const {
-    printBFS(root);
+    printBFS(ptrRoot);
 }
 
-void BinarySearchTree::printBFS(TreeNode* node) const {
-    if (!node) {
+void BinarySearchTree::printBFS(TreeNode* ptrNode) const {
+    if (!ptrNode) {
         return;
     }
 
     std::vector<TreeNode*> queue;
-    queue.push_back(node);
+    queue.push_back(ptrNode);
 
     while (!queue.empty()) {
         TreeNode* curr = queue.front();
         queue.erase(queue.begin());
 
-        std::cout << curr->data << " ";
+        std::cout << curr->intData << " ";
 
-        if (curr->left) {
-            queue.push_back(curr->left);
+        if (curr->ptrLeft) {
+            queue.push_back(curr->ptrLeft);
         }
-        if (curr->right) {
-            queue.push_back(curr->right);
+        if (curr->ptrRight) {
+            queue.push_back(curr->ptrRight);
         }
     }
 
@@ -142,43 +142,44 @@ void BinarySearchTree::printBFS(TreeNode* node) const {
 
 // Obtém a altura da árvore
 int BinarySearchTree::getHeight() const {
-    return getHeight(root);
+    return getHeight(ptrRoot);
 }
 
-int BinarySearchTree::getHeight(TreeNode* node) const {
-    if (!node) {
+int BinarySearchTree::getHeight(TreeNode* ptrNode) const {
+    if (!ptrNode) {
         return 0;
     }
-    int leftHeight = getHeight(node->left);
-    int rightHeight = getHeight(node->right);
-    return std::max(leftHeight, rightHeight) + 1;
+    int ptrLeftHeight = getHeight(ptrNode->ptrLeft);
+    int ptrRightHeight = getHeight(ptrNode->ptrRight);
+    return std::max(ptrLeftHeight, ptrRightHeight) + 1;
 }
 
 // Obtém o tamanho da árvore (número de nós)
 int BinarySearchTree::getSize() const {
-    return getSize(root);
+    return getSize(ptrRoot);
 }
 
-int BinarySearchTree::getSize(TreeNode* node) const {
-    if (!node) {
+int BinarySearchTree::getSize(TreeNode* ptrNode) const {
+    if (!ptrNode) {
         return 0;
     }
-    return getSize(node->left) + getSize(node->right) + 1;
+    return getSize(ptrNode->ptrLeft) + getSize(ptrNode->ptrRight) + 1;
 }
 
 // Converte a árvore em uma lista ordenada
 std::vector<int> BinarySearchTree::toList() const {
-    std::vector<int> list;
-    convertToList(root, list);
-    return list;
+    std::vector<int> vecList;
+    convertToList(ptrRoot, vecList);
+    return vecList;
 }
 
-void BinarySearchTree::convertToList(TreeNode* node, std::vector<int>& list) const {
-    if (!node) {
+void BinarySearchTree::convertToList(TreeNode* ptrNode, std::vector<int>& vecList) const {
+    if (!ptrNode) {
         return;
     }
-    convertToList(node->left, list);
-    list.push_back(node->data);
-    convertToList(node->right, list);
+    convertToList(ptrNode->ptrLeft, vecList);
+    vecList.push_back(ptrNode->intData);
+    convertToList(ptrNode->ptrRight, vecList);
 }
+
 
