d775e27977fa026b27a5d7b4b22ca7cc3799de93
Tulio Koneçny <61877782+TulioKonecny@users.noreply.github.com>
2023-06-30 16:57:04 -0300

    Atualização nos testes
    Adição de testes

--- DriverCodeBST.cpp
+++ DriverCodeBST.cpp
@@ -1,9 +1,12 @@
 #include "BinarySearchTree.h"
+#include "TreeExplorer.h"
+
 
 int main() {
     BinarySearchTree tree;
 
     // Insere elementos na árvore
+     std::cout << "Inserindo: 50 30 20 40 70 60 80" << std::endl;
     tree.insert(50);
     tree.insert(30);
     tree.insert(20);
@@ -13,27 +16,46 @@ int main() {
     tree.insert(80);
 
     // Realiza operações na árvore
+
+    // Imprime a árvore em ordem de largura (BFS)
+    std::cout << "Árvore em BFS:" << std::endl;
     tree.printBFS();
+
+    // Altura da árvore
     std::cout << "Altura da árvore: " << tree.getHeight() << std::endl;
+
+    // Quantidade de Elementos na árvore
     std::cout << "Número de elementos na árvore: " << tree.getSize() << std::endl;
 
-    TreeNode* node = tree.search(40);
-    if (node) {
+    //Busca do endereço de memória de um elemento
+    TreeNode* ptrNode1 = tree.search(40);
+    if (ptrNode1) {
         std::cout << "Elemento 40 encontrado na árvore." << std::endl;
     } else {
         std::cout << "Elemento 40 não encontrado na árvore." << std::endl;
     }
 
+    //Busca do endereço de memória de um elemento
+    TreeNode* ptrNode2 = tree.search(100);
+    if (ptrNode2) {
+        std::cout << "Elemento 100 encontrado na árvore." << std::endl;
+    } else {
+        std::cout << "Elemento 100 não encontrado na árvore." << std::endl;
+    }
+    
+    //Remoção de um elemento da árvore
+    std::cout << "Remoção do 40." << std::endl;
     tree.remove(40);
+    tree.printBFS();
 
-    std::vector<int> list = tree.toList();
+
+    std::vector<int> vecList = tree.toList();
     std::cout << "Lista de elementos da árvore: ";
-    for (int element : list) {
-        std::cout << element << " ";
+    for (int intElement : vecList) {
+        std::cout << intElement << " ";
     }
     std::cout << std::endl;
-
-    tree.clear();
+    
 
     return 0;
 }
